e0cb708ab306e0f8c765cb95e13a4ebd967b5d69
thierryskoda <skodathierry@hotmail.ca>
2016-08-12 15:17:59 -0400

    first push

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if(process.env.NODE_ENV == 'development') {
+  require('dotenv').config();
+}
 
 var express = require('express'),
   config = require('./config/config'),
